8c946a5dfd4090eeb3e0a826a052a9e20f0c9c62
Fabrizio Jurado <estefano.jurado.c52@gmail.com>
2024-03-19 10:21:14 -0500

    mapeo_especificaciones

---
+++ src/java/Modelo/Especificaciones.java
@@ -0,0 +1,53 @@
+package Modelo;
+
+public class Especificaciones {
+
+    private int id_esp;
+    private String titulo;
+    private String descripcion;
+    private int id_prod;
+ 
+   
+    public Especificaciones() {
+    }
+
+    public Especificaciones(int id_esp, String titulo, String descripcion, int id_prod) {
+        this.id_esp = id_esp;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.id_prod = id_prod;
+    }
+
+    public int getid_esp() {
+        return id_esp;
+    }
+
+    public void setid_esp(int id_esp) {
+        this.id_esp = id_esp;
+    }
+
+    public String gettitulo() {
+        return titulo;
+    }
+
+    public void settitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getdescripcion() {
+        return descripcion;
+    }
+
+    public void setdescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int id_prod() {
+        return id_prod;
+    }
+
+    public void setid_prod(int id_prod) {
+        this.id_prod = id_prod;
+    }
+ 
+}
